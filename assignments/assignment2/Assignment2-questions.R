3540b6a929e0459a87180c1ffaa8830dd4afb40e
Liz Lucas <elizabethklucas@gmail.com>
2021-09-08 09:41:26 -0500

    add assignment2

---
+++ assignments/assignment2/Assignment2-questions.R
@@ -0,0 +1,67 @@
+              #### ASSIGNMENT 2 ####
+
+# Answer the following questions using the dispatch.csv file 
+# and the tidyverse functions we've learned 
+
+
+# Question 1: What's the timeframe that this dataset covers?  
+
+# Your code: 
+
+
+
+
+# The answer
+
+
+
+
+# Question 2: Which day of the week had the most incidents? 
+
+# Your code: 
+
+
+
+
+# The answer: 
+
+
+
+
+# Question 3:  Which hour of the day had the most incidents?
+
+# Your code: 
+
+
+
+
+# The answer: 
+
+
+
+
+# Question 4:  Which police district had the most traffic stops?
+
+# Your code: 
+
+
+
+
+# The answer:
+
+
+
+
+
+# Question 5:  What's the most common reason police are dispatched to the airport? (11300 S AIRPORT DR)
+
+# Your code: 
+
+
+
+
+# The answer:
+
+
+
+
